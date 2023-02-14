d4079633e4ead46837c974f1b0c96af7928680a6
labradorescence <orangenewyorkcreativeagency@gmail.com>
2023-02-14 10:26:55 -0500

    prac

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,14 @@
 class StudentsController < ApplicationController
 
   def index
+    #show all the instances
     students = Student.all
     render json: students
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
